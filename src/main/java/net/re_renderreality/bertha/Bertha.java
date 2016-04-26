9b675662512eda4b4d3651ff9ffc7c6bc3216938
Alex Hoffmann <alexhoff@umich.edu>
2016-04-25 22:51:04 -0400

    Initial Commit

---
+++ src/main/java/net/re_renderreality/bertha/Bertha.java
@@ -0,0 +1,15 @@
+package net.re_renderreality.bertha;
+
+import org.spongepowered.api.plugin.Plugin;
+
+import me.flibio.updatifier.Updatifier;
+
+@Updatifier(repoName = "RRRP2", repoOwner = "ReRenderReality", version = "v" + PluginInfo.VERSION)
+@Plugin(id = "rrr.commands", name = "RRRP2", version = "0.0.1", description = "basic commands for RRP2 Bundle") 
+public class Bertha {
+	
+		
+	protected Bertha() {
+			
+	}
+}
---
+++ src/main/java/net/re_renderreality/bertha/PluginInfo.java
@@ -0,0 +1,12 @@
+package net.re_renderreality.bertha;
+
+/**
+ * General Plugin Info
+ */
+public class PluginInfo {
+	public static final String ID =  "rrr.bertha";
+	public static final String NAME = "Big Bertha";
+	public static final String VERSION = "@project.version@";
+	public static final String INFORMATIVE_VERSION = "@project.informativeVersion@";
+	public static final String DESCRIPTION = "@project.version@";
+}
